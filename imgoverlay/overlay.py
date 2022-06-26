16483891be48d818c2a4fb09cb6508e7f8581dfe
Jeremy Cohen <jcohen02@users.noreply.github.com>
2022-06-26 14:58:00 +0100

    Fixing PEP8 syntax.

--- imgoverlay/overlay.py
+++ imgoverlay/overlay.py
@@ -1,4 +1,5 @@
-from PIL import Image, ImageDraw, ImageFont, ImageColor
+from PIL import Image, ImageDraw, ImageFont
+
 
 class ImageOverlay:
     image_file = None
@@ -7,18 +8,19 @@ class ImageOverlay:
         'black': (0, 0, 0),
         'red': (255, 0, 0),
         'blue': (0, 0, 255),
-        'green': (0,255,0),
-        'white': (255,255,255) }
-    
+        'green': (0, 255, 0),
+        'white': (255, 255, 255),
+        'ornge': (240, 120, 70)
+    }
+
     text_colour = TEXT_COLOURS['black']
-    
+
     def __init__(self, image_file):
         self.image_file = image_file[0]
 
     def set_style(self, text_size, set_text_colour=None):
         if set_text_colour:
-            self.text_colour=set_text_colour
-
+            self.text_colour = set_text_colour
 
     def overlay_text(self, text):
         print('Image file: %s' % self.image_file)
@@ -26,9 +28,11 @@ class ImageOverlay:
             image_draw = ImageDraw.Draw(image)
             image_font = ImageFont.truetype('HelveticaNeue.ttc', 72)
             print('<%s>' % text)
-            image_draw.text((150,100), text[0], self.text_colour, font=image_font)
+            image_draw.text((150, 100), text[0], self.text_colour,
+                            font=image_font)
 
             extn_loc = self.image_file.rfind('.')
-            output_file_name = self.image_file[:extn_loc] + '_overlay' + self.image_file[extn_loc:]
+            output_file_name = (self.image_file[:extn_loc] +
+                                '_overlay' + self.image_file[extn_loc:])
 
             image.save(output_file_name)
