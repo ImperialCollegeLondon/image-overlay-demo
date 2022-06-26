00c15b2f09fd3d2f50d14e1f5d52d0520711829b
Jeremy Cohen <jcohen02@users.noreply.github.com>
2022-06-26 14:13:41 +0100

    Adding example testing setup

--- imgoverlay/overlay.py
+++ imgoverlay/overlay.py
@@ -10,10 +10,15 @@ class ImageOverlay:
         'green': (0,255,0),
         'white': (255,255,255) }
     
-    text_colour = TEXT_COLOURS['black']
+    text_colour = 'black'
+    text_size = 72
     
     def __init__(self, image_file):
         self.image_file = image_file[0]
+
+    def set_text_colour(self, set_text_colour):
+        if set_text_colour:
+            self.text_colour=set_text_colour
 
     def set_style(self, text_size, set_text_colour=None):
         if set_text_colour:
@@ -24,9 +29,9 @@ class ImageOverlay:
         print('Image file: %s' % self.image_file)
         with Image.open(self.image_file) as image:
             image_draw = ImageDraw.Draw(image)
-            image_font = ImageFont.truetype('HelveticaNeue.ttc', 72)
+            image_font = ImageFont.truetype('HelveticaNeue.ttc', self.text_size)
             print('<%s>' % text)
-            image_draw.text((150,100), text[0], self.text_colour, font=image_font)
+            image_draw.text((150,100), text[0], self.TEXT_COLOURS[self.text_colour], font=image_font)
 
             extn_loc = self.image_file.rfind('.')
             output_file_name = self.image_file[:extn_loc] + '_overlay' + self.image_file[extn_loc:]
